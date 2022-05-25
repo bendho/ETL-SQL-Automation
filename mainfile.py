b6a2c297741659bb1dc2418ad32f92aa0105dbf8
bendho <benjamindanielhollingsworth2@gmail.com>
2022-05-25 11:02:33 -0400

    we do a little bit of code cleaning

--- mainfile.py
+++ mainfile.py
@@ -5,7 +5,10 @@ import PySimpleGUI as sg
 import os 
 
 #Imported dataframe from the user.
-imported_df = pd.DataFrame
+imported_df = pd.read_csv("reference_tables/dummy.csv", encoding="latin-1", on_bad_lines='skip')
+
+#Imported DataFrame post modifications.
+modified_df = pd.read_csv("reference_tables/dummy.csv", encoding="latin-1", on_bad_lines='skip')
 
 #Reference tables. I need to look into adding more of these later on.
 
@@ -36,6 +39,8 @@ def find_area_rows (area_to_find, geotype_of_area):
 
 
 #This is a questionable way of coding this, but it works. I should take a look at this later if I can think of something. 
+
+#Add safety checks
     if (geotype_of_area == False or not geotype_of_area):
         rows_of_intrest = fips_df[fips_df[compare_choice].str.casefold().isin([x.casefold() for x in area_to_find])]
     else :
@@ -62,73 +67,72 @@ def isolate_data(checked_rows, type_of_data) :
     return parsed_data
 
 
-def export_file(data_to_export):
-
-    export_file_question = input("export file? This may overwrite any existing files in the export folder with the same name. y/n ")
-    
-    if export_file_question != "y":
-        return
+def export_file():
+    global modified_df
     
     export_path_input = input("Enter a file name to use, files will be exported into the exports folder. ")
 
     if(not export_path_input):
         print("You did not input a valid path.")
         return False
-    
 
     cwd = os.getcwd()
 
     export_path = cwd + "/exports/" + export_path_input + ".csv"
 
-    data_to_export.to_csv(export_path)
+    modified_df.to_csv(export_path)
     print("csv was successfuly exported to " + export_path)
 
 # Handles the conversion of actual CSV files.
 def file_input():
+    global imported_df
+    global modified_df
+
+    print(imported_df.columns)
+
+    ref_column = input("Chose a column to use as a reference... ")
+    modified_df = imported_df.loc[: , ref_column]
+
+    if modified_df.empty :
+        print("The column you chose lacks data.")
+        return
+    
+    print(modified_df)
+
+    geo_input = input("Enter a geotype, if none is entered everything will show.... ")
+
+    modified_df = find_area_rows(modified_df, geo_input)
+
+    print(modified_df)
+
+    choice = input("Would you like to further prune your selection? y/n ")
+
+    if choice == "y":
+        column_list = modified_df.columns.values.tolist()
+        print(column_list)
+        prune_choices = input("Enter the columns that you would like to seperate. use a blank space to seperate your inputs. ")
+        prune_choices = prune_choices.split(" ")
+        modified_df = isolate_data(modified_df, prune_choices)
+        print(modified_df)
+    
+    choice = input("export file? This may overwrite any existing files in the export folder with the same name. y/n ")
+
+    if choice =="y":
+        export_file()
+
+def import_csv():
     inputed_file = input("Input the path for the file that you want to use... ")
 
     if not ".csv" in inputed_file:
         print("the file you picked was not a csv. please use a csv.")
         return False
-    
-    reference_df = pd.read_csv(inputed_file, encoding="latin-1", on_bad_lines='skip')
-    
-    print(reference_df.columns)
 
-    ref_column = input("Chose a column to use as a reference... ")
-    reference_df = reference_df.loc[: , ref_column]
-
-    if reference_df.empty :
-        print("The column you chose lacks data.")
-        return
-    
-    print(reference_df)
-
-    geo_input = input("Enter a geotype, if none is entered everything will show.... ")
-
-    found_rows = find_area_rows(reference_df, geo_input)
-
-    print(found_rows)
-
-    choice = input("Would you like to further prune your selection? y/n ")
-
-    if choice == "y":
-        column_list = found_rows.columns.values.tolist()
-        print(column_list)
-        prune_choices = input("Enter the columns that you would like to seperate. use a blank space to seperate your inputs. ")
-        prune_choices = prune_choices.split(" ")
-        pruned_data = isolate_data(found_rows, prune_choices)
-        print(pruned_data)
-
-        found_rows = pruned_data
-    
-    choice = input("Do you want to export this file? y/n ")
-
-    if choice =="y":
-        export_file(pruned_data)
+    global imported_df
+    imported_df = pd.read_csv(inputed_file, encoding="latin-1", on_bad_lines='skip')
 
 #This is used as a launchingpad to reach other parts of the program
 def main_function() :
+    import_csv()
     file_input()
 
 main_function()
