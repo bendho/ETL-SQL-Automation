3b5b55c8800ec1924e03c70ec95c29148d55c2e5
bendho <benjamindanielhollingsworth2@gmail.com>
2022-06-02 12:42:58 -0400

    pivoting

--- mainfile.py
+++ mainfile.py
@@ -211,6 +211,35 @@ def unpivot():
     modified_df = pd.melt(modified_df, id_vars = safe_columns, value_vars = user_input_columns, value_name = user_value_name, var_name = user_var_name)
     print(modified_df)        
 
+def pivot():
+    global modified_df
+
+    if modified_df is False:
+        print("There is not a database to export. Please import a file.")
+        return False
+
+    column_list = modified_df.columns.values.tolist()
+    print(column_list)
+
+    user_input_value = input("Please enter value column that you'd like to pivot... ")
+
+    if user_input_value not in column_list:
+        print("The Column you inputed does not exist.")
+        return False
+ 
+    user_input_column = input("Please enter the column that you'd like to turn into rows.... ")
+
+    if user_input_column not in column_list:
+        print("The Column you inputed does not exist.")
+        return False
+    
+    safe_columns = [x for x in column_list if x not in user_input_column]
+    safe_columns = [x for x in safe_columns if x not in user_input_value]
+
+    modified_df = modified_df.pivot_table(index=safe_columns, values = user_input_value, columns= user_input_column)            
+    print(modified_df)
+
+
 def prune_df_rows():
     global modified_df
 
@@ -280,6 +309,7 @@ def csv_menu():
     print("8. Chose a table to use as a reference")
     print("9. Sort Columns")
     print("10. Unpivot Columns")
+    print("11. Pivot Columns")
     menu_choice = input("choose a menu.... ")
 
     match menu_choice:
@@ -305,6 +335,8 @@ def csv_menu():
             swap_columns()
         case '10':
             unpivot()
+        case '11':
+            pivot()
 
     print(" ")
     csv_menu() 
